47a88887b6540156dda9e88a91aa6add88af2549
Gustavo <gustavo.santos0106@gmail.com>
2014-12-16 19:48:59 -0200

    synapse: external storage settings

--- usr/galaxys2_initramfs_files/res/synapse/config.json.generate.fs
+++ usr/galaxys2_initramfs_files/res/synapse/config.json.generate.fs
@@ -3,7 +3,7 @@ cat << CTAG
     name:I/O,
     elements:[
     { STitleBar:{
-		title:"I/O Settings"
+		title:"Internal Storage Settings"
 	}},
 	{ SOptionList:{
 		title:"I/O Scheduler",
@@ -23,6 +23,36 @@ cat << CTAG
 		default:128,
                 action:"generic /sys/devices/virtual/bdi/179:0/read_ahead_kb",
 	}},
+{ SPane:{
+		title:"External Storage Settings"
+	}},	
+	{ SOptionList:{
+		title:"I/O Scheduler",
+		default:sio,
+		action:"bracket-option /sys/block/mmcblk1/queue/scheduler",
+		values:[
+`
+			for IOSCHED in \`cat /sys/block/mmcblk1/queue/scheduler | sed -e 's/\]//;s/\[//'\`; do
+			  echo "\"$IOSCHED\","
+			done
+`
+		]
+	}},
+	{ SSeekBar:{
+		title:"I/O read-ahead buffer",
+		max:4096, min:128, unit:" kB", step:128,
+		default:128,
+                action:"generic /sys/block/mmcblk1/queue/read_ahead_kb",
+	}},
+{ SPane:{
+		title:"Dynamic Fsync"
+	}},
+{ SCheckBox:{
+        label:"Dynamic Fsync",
+        description:"Makes Fsync operation asynchronous with screen on, increasing speed but decreasing data integrity if a freeze happens or if battery is removed. With screen off, Fsync operation is synchronous.",
+        default:0,
+        action:"generic /sys/kernel/dyn_fsync/Dyn_fsync_active"
+    }},
    ]
 }
 
