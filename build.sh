3aa567f994e9f9eb9187576a13d8d08a6f5c5089
Gustavo <gustavo.santos0106@gmail.com>
2015-01-02 23:48:50 -0200

    build.sh: Initial lollipop support

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 TOOLCHAIN="/home/gustavo/toolchain/bin/arm-cortex_a9-linux-gnueabihf-"
 STRIP="/home/gustavo/toolchain/bin/arm-cortex_a9-linux-gnueabihf-strip"
 OUTDIR="out"
-KK_CWM_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cwm.list"
-KK_TWRP_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/twrp.list"
-JB_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/jb.list"
+CWM_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cwm.list"
+TWRP_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/twrp.list"
 RAMDISK="/home/gustavo/ramdisk"
+RAMDISK_LP="/home/gustavo/ramdisk_lp"
 RAMDISK_OUT="/home/gustavo/kernel/usr/initramfs/ramdisk.cpio"
 MODULES=("/home/gustavo/kernel/net/sunrpc/auth_gss/auth_rpcgss.ko" "/home/gustavo/kernel/fs/cifs/cifs.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "/home/gustavo/kernel/fs/lockd/lockd.ko" "/home/gustavo/kernel/fs/nfs/nfs.ko" "/home/gustavo/kernel/net/sunrpc/auth_gss/rpcsec_gss_krb5.ko" "drivers/scsi/scsi_wait_scan.ko" "drivers/samsung/fm_si4709/Si4709_driver.ko" "/home/gustavo/kernel/net/sunrpc/sunrpc.ko")
 KERNEL_DIR="/home/gustavo/kernel"
@@ -37,14 +37,14 @@ case "$1" in
       
         # build the CWM kernel
         cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_CWM_INITRAMFS_SOURCE}
+        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${CWM_INITRAMFS_SOURCE}
         cp arch/arm/boot/zImage ${OUTDIR}
         cd ${OUTDIR}
 		echo "Creating kk CWM kernel zip..."
         zip -r kk-kernel-$CURRENTDATE-CWM.zip ./ -x *.zip /system/* *.gitignore
         # build the TWRP kernel
         cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${KK_TWRP_INITRAMFS_SOURCE}
+        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${TWRP_INITRAMFS_SOURCE}
         cp arch/arm/boot/zImage ${OUTDIR}
         cd ${OUTDIR}
 		echo "Creating kk TWRP kernel zip..."
@@ -52,7 +52,12 @@ case "$1" in
         
 		echo "Done!"
 	    ;;
-	  jb)
+	  lp)
+	    # compress the ramdisk in cpio
+        cd ${RAMDISK_LP}
+        rm *.cpio
+        find . -not -name ".gitignore" | cpio -o -H newc > ${RAMDISK_OUT}
+        
 	    cd ${KERNEL_DIR}
         make kernel_defconfig ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
 
@@ -64,13 +69,21 @@ case "$1" in
             ${STRIP} --strip-unneeded ${MODULES_DIR}/*
         done
       
-        # build the jelly bean kernel
+        # build the CWM lollipop kernel
         cd ${KERNEL_DIR}
-        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${JB_INITRAMFS_SOURCE} zImage
+        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${CWM_INITRAMFS_SOURCE} zImage
         cp arch/arm/boot/zImage ${OUTDIR}
         cd ${OUTDIR}
-		echo "Creating jb kernel zip..."
-        zip -r jb-kernel-$CURRENTDATE.zip ./ -x *.zip *.gitignore
+		echo "Creating lp kernel zip..."
+        zip -r lp-kernel-$CURRENTDATE-CWM.zip ./ -x *.zip *.gitignore
         
-        echo "Done!"
+         # build the TWRP lollipop kernel
+        cd ${KERNEL_DIR}
+        make -j3 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${TWRP_INITRAMFS_SOURCE}
+        cp arch/arm/boot/zImage ${OUTDIR}
+        cd ${OUTDIR}
+		echo "Creating kk TWRP kernel zip..."
+        zip -r lp-kernel-$CURRENTDATE-TWRP.zip ./ -x *.zip /system/* *.gitignore
+        
+		echo "Done!"
 esac
